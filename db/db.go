483ab07dfa3b6eca0c01397e6a1437ff71911b15
Haruki Miyagi <haruki.miyagi11@gmail.com>
2024-01-20 13:56:06 +0900

    Load local .env file and display error before terminating program

--- db/db.go
+++ db/db.go
@@ -12,14 +12,15 @@ import (
 
 func NewDB() *gorm.DB {
 	if os.Getenv("GO_ENV") == "dev" {
-		err := godotenv.Load()
+		err := godotenv.Load() // ローカルの.env ファイルを読む設定
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalln(err) // エラーを表示してからプログラムを強制終了させる
 		}
 	}
 	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+	// &gorm.Config{}: デフォルトの設定
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
