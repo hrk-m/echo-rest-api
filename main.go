753d85ed83d3272867dfa4deddb9d95f184890b1
Haruki Miyagi <haruki.miyagi11@gmail.com>
2024-01-26 21:31:57 +0900

    Add initial main.go file with basic setup

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"go-rest-api/controller"
+	"go-rest-api/db"
+	"go-rest-api/repository"
+	"go-rest-api/router"
+	"go-rest-api/usecase"
+)
+
+func main() {
+	db := db.NewDB()
+	userRepository := repository.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	userController := controller.NewUserController(userUsecase)
+	e := router.NewRouter(userController)
+	e.Logger.Fatal(e.Start(":8080"))
+}
