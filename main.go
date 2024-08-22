7ef0be23116970816dae07c12d70f3b56361be1a
Haruki Miyagi <haruki.miyagi11@gmail.com>
2024-08-22 16:15:46 +0900

    Refactor code for better readability and scalability

--- main.go
+++ main.go
@@ -13,12 +13,17 @@ func main() {
 	db := db.NewDB()
 	userValidator := validator.NewUserValidator()
 	taskValidator := validator.NewTaskValidator()
+
 	userRepository := repository.NewUserRepository(db)
 	taskRepository := repository.NewTaskRepository(db)
+
 	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
 	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
+
 	userController := controller.NewUserController(userUsecase)
 	taskController := controller.NewTaskController(taskUsecase)
+
 	e := router.NewRouter(userController, taskController)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
